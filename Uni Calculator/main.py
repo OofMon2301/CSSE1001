8a4d462f7be6cf5a8aa50ec54487dafca7080f40
Jamie Chen <jamiechen2301@gmail.com>
2023-07-01 20:34:08 +1000

    Updating Entries

--- Uni Calculator/main.py
+++ Uni Calculator/main.py
@@ -42,8 +42,8 @@ def enter_button_clicked():
         course_entries.append(course_entry)
 
     # Save Button
-    save_button = customtkinter.CTkButton(app, text="Save", command=save_courses, font=("Arial", 15))
-    save_button.pack(padx=10, pady=15)
+    save_button = customtkinter.CTkButton(app, text="Save", command=save_courses, font=("Arial", 20), width=15, height=2)
+    save_button.pack(pady=20)
 
 def save_courses():
     course_names = [entry.get() for entry in course_entries]
@@ -53,9 +53,36 @@ def save_courses():
         for course_name in course_names:
             file.write(course_name + "\n")
 
-    # Update the labels with the edited course names
-    for i in range(num_courses):
-        course_labels[i].configure(text=course_names[i])
+    # Destroy Previous Page
+    for label in course_labels:
+        label.pack_forget()
+    for entry in course_entries:
+        entry.pack_forget()
+
+    # Create Overview with Course Buttons
+    for i, course_name in enumerate(course_names):
+        course_button = customtkinter.CTkButton(app, text=course_name, font=("Arial", 15), width=20, height=2)
+        course_button.pack(side="left", padx=10, pady=10)
+
+        add_assignment_button = customtkinter.CTkButton(app, text="Add Assignment", font=("Arial", 12), width=15)
+        add_assignment_button.pack(side="left", padx=10, pady=10)
+
+def load_courses():
+    # Read the previously saved course names
+    try:
+        with open("course_names.txt", "r") as file:
+            course_names = file.readlines()
+        course_names = [name.strip() for name in course_names]
+    except FileNotFoundError:
+        course_names = []
+
+    # Create Overview with Course Buttons
+    for i, course_name in enumerate(course_names):
+        course_button = customtkinter.CTkButton(app, text=course_name, font=("Arial", 15), width=20, height=2)
+        course_button.pack(side="left", padx=10, pady=10)
+
+        add_assignment_button = customtkinter.CTkButton(app, text="Add Assignment", font=("Arial", 12), width=15)
+        add_assignment_button.pack(side="left", padx=10, pady=10)
 
 # Update the UI for enter_button
 enter_button = customtkinter.CTkButton(app, text="Enter", command=enter_button_clicked, font=("Arial", 15))
@@ -77,6 +104,9 @@ enter_button.pack(padx=10, pady=15)
 # System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
+
+# Load the saved course information
+load_courses()
 
 # Run App
 app.mainloop()
